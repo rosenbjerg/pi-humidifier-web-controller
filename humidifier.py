85b7385c8eac388ed1f3973011041d3d7d40f494
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-12-09 22:51:39 +0100

    Add Home Assistant "support"

--- humidifier.py
+++ humidifier.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+from dotenv import load_dotenv
 from flask import Flask
 from threading import Event, Thread
 import RPi.GPIO as GPIO
+import requests
+import os
 
 buttonPin = 23
 
@@ -14,12 +17,25 @@ humidifying_ended = Event()
 humidifying = False
 
 
+load_dotenv()
+hass_token = os.getenv('HASS_TOKEN')
+hass_entity_id = os.getenv('HASS_ENTITY_ID')
+hass_host = os.getenv('HASS_HOST')
+
+def report_to_homeassistant(state):
+    global hass_token
+    global hass_entity_id
+    global hass_host
+    if hass_token is not None:
+        requests.post(f'http://{hass_host}/api/states/{hass_entity_id}', headers={'Authorization': f'Bearer {hass_token}'}, json={"state": state, "attributes": {"friendly_name": 'Plant Humidifier', "icon": 'mdi:sprinkler-variant'}})
+
 def initialize():
     print('initializing')
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(buttonPin, GPIO.OUT)
-
     GPIO.output(buttonPin, GPIO.LOW)
+
+    report_to_homeassistant('Off')
 
 
 def turn_on_humidifier():
@@ -39,6 +55,7 @@ def turn_on():
 
     humidifying_ended.clear()
     humidifying = True
+    report_to_homeassistant('On')
 
     for i in range(rounds):
         if not humidifying:
@@ -54,6 +71,7 @@ def turn_on():
 
     print('humidification completed')
     turn_off_humidifier()
+    report_to_homeassistant('Off')
     humidifying = False
 
 
@@ -62,6 +80,7 @@ def turn_off():
     global humidifying
     humidifying = False
     humidifying_ended.set()
+    report_to_homeassistant('Off')
 
 
 app = Flask(__name__)
@@ -85,7 +104,9 @@ def humidifier_off():
 
 
 if __name__ == '__main__':
-    initialize()
-    print('starting hosting')
-    app.run(host='0.0.0.0')
-    GPIO.cleanup()
+    try:
+        initialize()
+        print('starting hosting')
+        app.run(host='0.0.0.0')
+    finally:
+        GPIO.cleanup()
